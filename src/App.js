8ee64fe2421ed821bda00d60fb0b43a4a54b810e
Stanislau Charnavalod <s.charnavalod@itechart-group.com>
2023-03-16 00:28:11 +0100

    create landing

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ class App extends Component {
               <Route path="/privacy-policy" name="PrivacyPolicy" render={ (props) => <PrivacyPolicy { ...props } /> }/>
               <Route exact path="/404" name="Page 404" render={ (props) => <Page404 { ...props } /> }/>
               <Route exact path="/500" name="Page 500" render={ (props) => <Page500 { ...props } /> }/>
-              <Route exact path="/" name="Landing" render={ (props) => <Landing { ...props } /> }/>
+              <Route exact path="/landing" name="Landing" render={ (props) => <Landing { ...props } /> }/>
               { !isLoggedIn && isBusinessUserType && <Redirect to="/login"/> }
               { !isLoggedIn && isBankUserType && <Redirect to="/bank/login"/> }
               { !isLoggedIn && <Redirect to="/login"/> }
